b5b85295587cb1f0c165be5139563846b1335208
Carlodavid Soto <92967639+Bebo561@users.noreply.github.com>
2024-05-02 10:24:29 -0400

    Removed sensitive information

--- Front-end/src/Firebase.tsx
+++ Front-end/src/Firebase.tsx
@@ -6,12 +6,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBH_FyWadKBU2soHo_t2QPXNieRzBg2NxI",
-  authDomain: "expresstrack-c6bf2.firebaseapp.com",
+  apiKey: "",
+  authDomain: "",
   projectId: "expresstrack-c6bf2",
-  storageBucket: "expresstrack-c6bf2.appspot.com",
-  messagingSenderId: "605080118629",
-  appId: "1:605080118629:web:81d5e002834d9946e36d8c"
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
